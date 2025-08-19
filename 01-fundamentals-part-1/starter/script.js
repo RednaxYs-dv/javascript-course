ae60a6d3d64cd1679baa78a753a94f671cff6d86
RednaxYs-dv <syalexander301@gmail.com>
2025-08-20 07:53:12 +0800

    test desc

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,8 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+// JavaScript Fundamentals - Hour 1
+// My first JavaScript code!
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
