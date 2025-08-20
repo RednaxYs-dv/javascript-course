1f96031b149fe807656635ce01dd928303042e01
RednaxYs-dv <syalexander301@gmail.com>
2025-08-20 08:16:40 +0800

    test 1111

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -3,3 +3,19 @@
 
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("=== VARIABLES ===");
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+age = 40; // We can change let variables
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+// birthYear = 1990;
+
+const PI  = 3.1415;
+console.log(PI);
